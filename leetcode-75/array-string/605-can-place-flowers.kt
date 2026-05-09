2d09f3a402e5e9217681d2df15757f88ee6ee6fc
Mikail Khamkhoev <mikailkhamkhoev@outlook.com>
2026-05-09 18:07:53 +0300

    feat: add solution for 605. Can Place Flowers

---
+++ leetcode-75/array-string/605-can-place-flowers.kt
@@ -0,0 +1,26 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+class Solution {
+    fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+        var count = 0
+        var i = 0
+        
+        while (i < flowerbed.size) {
+            if (flowerbed[i] == 0) {
+                val prevEmpty = (i == 0 || flowerbed[i - 1] == 0)
+                val nextEmpty = (i == flowerbed.size - 1 || flowerbed[i + 1] == 0)
+                
+                if (prevEmpty && nextEmpty) {
+                    flowerbed[i] = 0
+                    count++
+                    i++
+                }
+            }
+            if (count >= n) return true
+            i++
+        }
+        
+        return count >= n
+    }
+}
