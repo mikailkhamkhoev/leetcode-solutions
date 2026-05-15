d80a3e8a5eda84bc4b0eef26c7447a6b88c7cd0a
Mikail Khamkhoev <mikailkhamkhoev@outlook.com>
2026-05-15 12:46:20 +0300

    feat: add solution for 2215. Find the Difference Between two Arrays

---
+++ leetcode-75/hash-map-set/2215-find-the-difference-between-two-arrays.kt
@@ -0,0 +1,14 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+class Solution {
+    fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
+        val set1 = nums1.toSet()
+        val set2 = nums2.toSet()
+
+        val answer1 = set1.filter { it !in set2 }
+        val answer2 = set2.filter { it !in set1 }
+
+        return listOf(answer1, answer2)
+    }
+}
