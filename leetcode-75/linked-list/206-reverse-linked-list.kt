886cf053c4ad5f516cdb7fa62a750ae2d0271523
Mikail Khamkhoev <mikailkhamkhoev@outlook.com>
2026-05-21 12:31:41 +0300

    feat: add solution for 206. Reverse Linked LIst

---
+++ leetcode-75/linked-list/206-reverse-linked-list.kt
@@ -0,0 +1,23 @@
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        var prev: ListNode? = null
+        var current = head
+
+        while (current != null) {
+            val next = current.next
+            current.next = prev
+
+            prev = current
+            current = next
+        }
+
+        return prev
+    }
+}
