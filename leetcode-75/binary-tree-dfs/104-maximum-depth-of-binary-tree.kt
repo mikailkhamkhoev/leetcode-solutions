39d3c1a60a5aa39ef69f71063efc1793228bc41d
Mikail Khamkhoev <mikailkhamkhoev@outlook.com>
2026-05-22 10:19:05 +0300

    feat: add solution for 104. Maximum Depth of Binary Tree

---
+++ leetcode-75/binary-tree-dfs/104-maximum-depth-of-binary-tree.kt
@@ -0,0 +1,14 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+        return 1 + maxOf(maxDepth(root.left), maxDepth(root.right))
+    }
+}
